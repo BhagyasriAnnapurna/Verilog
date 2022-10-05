ee5cc8d5d037fdfdb33f285fee76e3d85b3de878
Bhagyasri Annapurna <105233033+BhagyasriAnnapurna@users.noreply.github.com>
2022-10-05 19:46:56 +0530

    Update JK_FF.v

--- JK_FF.v
+++ JK_FF.v
@@ -1,6 +1,6 @@
 module JK_FF(clk,rst,J,K,q,qb);
    input clk,rst,J,K;
-   output reg q,qb;
+   output reg q;
    output qb;
    assign qb=~q;
    always @(posedge clk)
@@ -11,10 +11,10 @@ module JK_FF(clk,rst,J,K,q,qb);
 	    q<=q;
 	  else if(J==0 && K==1)
 	    q<=0;
-      else if(J==1 && K==0)
+          else if(J==1 && K==0)
 	    q<=1;
 	  else if(J==1 && K==1)
-		q<=~q;	
+            q<=~q;	
 	end
 endmodule
      
